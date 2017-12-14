16eb6ee8f909fd262b920d688bb8c2346326b437
Colton Gardner <cgardner268@gmail.com>
2017-12-14 12:33:40 -0800

    added unit test for the all the datacache methods

--- BankingConsoleApp.Tests/DataCacheTests.cs
+++ BankingConsoleApp.Tests/DataCacheTests.cs
@@ -25,14 +25,53 @@ namespace BankingConsoleApp.Tests
         [TearDown]
         public void Cleanup()
         {
-            testBankAccount = null;
+            dataCacheInstance.GetBankAccounts(null).RemoveAll(acct => acct != null);
         }
 
         [Test]
-        public void LoginToTestBankAccount()
+        public void AddTestBankAccountToCache()
         {
-
+            dataCacheInstance.AddBankAccount(testBankAccount);
+            Assert.That(dataCacheInstance.GetBankAccounts(null).Count == 1);
         }
 
+        [Test]
+        public void GetBalanceForTestBankAccount()
+        {
+            dataCacheInstance.AddBankAccount(testBankAccount);
+            Assert.That(dataCacheInstance.GetBankAccounts(1).Select(acct => acct).First().Balance == 0.0m);
+        }
+
+        [Test]
+        public void DepositToTestBankAccount()
+        {
+            dataCacheInstance.AddBankAccount(testBankAccount);
+            dataCacheInstance.Deposit(1, 50.90m);
+            Assert.That(dataCacheInstance.GetBankAccounts(1).Select(acct => acct).First().Balance == 50.90m);
+        }
+
+        [Test]
+        public void WithdrawlFromTestBankAccount()
+        {
+            dataCacheInstance.AddBankAccount(testBankAccount);
+            dataCacheInstance.Deposit(1, 100.0m);
+            dataCacheInstance.Withdrawl(1, 60.20m);
+            Assert.That(dataCacheInstance.GetBankAccounts(1).Select(acct => acct).First().Balance == 39.80m);
+        }
+
+        [Test]
+        public void OverdraftWithdrawlFromTestBankAccount()
+        {
+            dataCacheInstance.AddBankAccount(testBankAccount);
+            dataCacheInstance.Deposit(1, 100.0m);
+            Assert.That(dataCacheInstance.Withdrawl(1, 200.0m) == false);
+        }
+
+        [Test]
+        public void GetTransactionHistoryForTestBankAccount()
+        {
+            dataCacheInstance.AddBankAccount(testBankAccount);
+            Assert.That(dataCacheInstance.GetBankAccounts(1).Select(acct => acct).First().TransactionLog[0].Contains("Account Test Account was created"));
+        }
     }
 }
